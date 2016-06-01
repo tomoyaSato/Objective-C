38dfe4ae0e4279aa1bae2a500593a0f229fd67ea
Minami Kyohei <minami@sac-gotou.sactyo03.sacn.co.jp>
2016-06-01 16:28:10 +0900

    データ型のサイズ取得

--- chapter8/Addresses/Addresses/main.c
+++ chapter8/Addresses/Addresses/main.c
@@ -12,6 +12,10 @@ int main(int argc, const char * argv[]) {
     int i = 17;
     int *addressOfI = &i; //iのアドレスをポインタ変数addressOfIに代入
     printf("i stores its value at %p\n", addressOfI);
-    
+    printf("the in stored at addressOfI is %d\n", *addressOfI);//逆参照演算子*でこのアドレスに格納している値を取得
+    *addressOfI = 89;//この場合addresOfIが示すアドレスへ89を代入
+    printf("Now i is %d\n", i); //iが格納されているアドレス（addressOfi）が89になったのでi = 89となる
+    printf("An int is %zu bytes \n", sizeof(int));
+    printf("A pointer is %zu bytes \n", sizeof(int *));
     return 0;
 }
