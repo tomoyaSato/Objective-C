2be77e08a72c723ea1447fe3f69f17b7f91ea0b5
Minami Kyohei <minami@sac-gotou.sactyo03.sacn.co.jp>
2016-06-01 14:29:05 +0900

    ５章関数

---
+++ AGoodStart/AGoodStart/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Minami Kyohei on 2016/06/01.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    //小説の冒頭部分を出力する
+    printf("It was the best fo thimes. \n");
+    printf("It was the worst fo times. \n");
+    /* こんな感じでどうだろう？
+    　　書き直す必要があると思うけど */
+}
