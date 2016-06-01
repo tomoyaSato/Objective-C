f8be12712a11a968ee476f95bd3207b65636289e
Minami Kyohei <minami@sac-gotou.sactyo03.sacn.co.jp>
2016-06-01 15:11:25 +0900

    ループ処理させたい部分を追加

---
+++ Coolness/Coolness/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Coolness
+//
+//  Created by Minami Kyohei on 2016/06/01.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    printf("Aaron is Cool\n");//ループ表示させたい
+    
+    return 0;
+}
