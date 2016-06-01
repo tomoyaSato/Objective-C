dc8dc2b4a0b18884d569dd2d94b4123a11db5615
Minami Kyohei <minami@sac-gotou.sactyo03.sacn.co.jp>
2016-06-01 17:47:51 +0900

    構造体で人間情報を持つ変数を作成しBMI計算 javaのインスタンス生成と似ている？

--- chapter10/heightInMeters/heightInMeters/main.c
+++ chapter10/heightInMeters/heightInMeters/main.c
@@ -8,8 +8,23 @@
 
 #include <stdio.h>
 
+//構造体の宣言
+typedef struct{
+    float heightInMeters;
+    int weightInkilos;
+}Person;
+
+float bodyMassIndex(Person p){
+    return p.weightInkilos / (p.heightInMeters * p.heightInMeters);
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+   
+    Person person;
+    person.weightInkilos = 96;
+    person.heightInMeters = 1.8;
+    float bmi = bodyMassIndex(person);
+    printf("person has a BMI of %f\n", bmi);
+    
     return 0;
 }
