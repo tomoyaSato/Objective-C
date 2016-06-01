2a14e438ba98bbfa8807960aa66719ed340cf510
Minami Kyohei <minami@sac-gotou.sactyo03.sacn.co.jp>
2016-06-01 15:07:16 +0900

    mathライブラリのsin関数を利用

---
+++ math/math/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  math
+//
+//  Created by Minami Kyohei on 2016/06/01.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, const char * argv[]) {
+    double num =  sin(1);
+    printf("sin is %.3f\n", num);
+    return 0;
+}
