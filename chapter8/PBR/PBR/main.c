00f473cac8b5c309a40fe6bebc4b359759645c3d
Minami Kyohei <minami@sac-gotou.sactyo03.sacn.co.jp>
2016-06-01 17:27:10 +0900

    参照渡し

---
+++ chapter8/PBR/PBR/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  PBR
+//
+//  Created by Minami Kyohei on 2016/06/01.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
