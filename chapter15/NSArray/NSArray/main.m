1f725bd4cf55baf6f7666dfeeadc80db775ab727
Minami Kyohei <minami@sacmac.local>
2016-06-02 13:27:32 +0900

    NSMutableArrayを利用し配列にデータを挿入、削除

--- chapter15/NSArray/NSArray/main.m
+++ chapter15/NSArray/NSArray/main.m
@@ -15,6 +15,24 @@ int main(int argc, const char * argv[]) {
         NSDate *tomorrow = [now dateByAddingTimeInterval:24.0 * 60.0 * 60.0];
         NSDate *yesterday = [now dateByAddingTimeInterval:-24.0 * 60.0 * 60.0];
         
+        NSMutableArray *dateList = [NSMutableArray array];
+        
+        [dateList addObject:now];
+        [dateList addObject:tomorrow];
+        
+        //配列の先にyesterdayを挿入する
+        [dateList insertObject:yesterday atIndex: 0];
+        
+        //高速列挙を使って表示
+        for(NSDate *d in dateList){
+            NSLog(@"Here is a date: %@", d);
+        }
+        
+        //先頭を削除
+        [dateList removeObjectAtIndex: 0];
+        NSLog(@"Now the first date is %@", [dateList objectAtIndex: 0]);
+        
+        /*
         //３つのオブジェクトを保持する配列を生成する（nilはリストの終端を意味する）
         NSArray *dateList = [NSArray arrayWithObjects:now, tomorrow, yesterday, nil];
         
@@ -23,7 +41,7 @@ int main(int argc, const char * argv[]) {
             NSDate *d = [dateList objectAtIndex:i];
             NSLog(@"The first date is %@",d);//objectAtIndexで配列の要素にアクセスできる
         }
-        
+        */
     }
     return 0;
 }
