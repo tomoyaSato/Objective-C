63a36b414fb6802cc56a9d413d779c45f3030d16
Minami Kyohei <minami@sac-gotou.sactyo03.sacn.co.jp>
2016-06-02 10:01:27 +0900

    引数のあるメソッド

--- chapter12/TimeAfterTime/TimeAfterTime/main.m
+++ chapter12/TimeAfterTime/TimeAfterTime/main.m
@@ -17,6 +17,12 @@ int main(int argc, const char * argv[]) {
         //%pはアドレスのトークン
         //%@はobject情報のトークン
         NSLog(@"The date is %@", now);
+        
+        double seconds = [now timeIntervalSince1970];//nowオブジェクトの中にtimeIntervalSince1970というメソッドがある？
+        NSLog(@"It has been %f seconds since the start of 1970.", seconds);
+        
+        NSDate *later = [now dateByAddingTimeInterval:100000];
+        NSLog(@"IN 100,000 seconds it will be %@", later);
     }
     return 0;
 }
