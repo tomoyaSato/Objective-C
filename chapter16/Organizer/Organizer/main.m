557ba00dcd1dd73e0e537e0e75709a3203df7bc2
Minami Kyohei <minami@sacmac.local>
2016-06-02 13:34:56 +0900

    organizerからディペロッパドキュメントを閲覧する

---
+++ chapter16/Organizer/Organizer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Organizer
+//
+//  Created by Minami Kyohei on 2016/06/02.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+     /*
+     この章ではorganizerからリファレンスの参照の仕方を学んだ 
+     */
+    }
+    return 0;
+}
