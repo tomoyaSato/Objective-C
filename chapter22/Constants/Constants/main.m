791ca8ae90f73562180df29f0142ef5d249a003f
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-03 12:55:08 +0900

    グローバル変数を扱う

--- chapter22/Constants/Constants/main.m
+++ chapter22/Constants/Constants/main.m
@@ -14,6 +14,11 @@ int main(int argc, const char * argv[]) {
         //maxは関数ではない
         NSLog(@"\u03c0 is %f", M_PI);
         NSLog(@"%d is larger", MAX(10,12));
+        
+        //グローバル変数
+        NSLocale *here = [NSLocale currentLocale];
+        NSString *currency = [here objectForKey:NSLocaleCurrencyCode];
+        NSLog(@"Money is %@", currency);
     }
     return 0;
 }
