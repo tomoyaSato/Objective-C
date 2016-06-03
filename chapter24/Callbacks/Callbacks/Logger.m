f21e26e9f4dc57185a624ec3a943391b40867f2b
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-03 14:23:35 +0900

    コールバックプログラム

---
+++ chapter24/Callbacks/Callbacks/Logger.m
@@ -0,0 +1,19 @@
+//
+//  Logger.m
+//  Callbacks
+//
+//  Created by Minami Kyohei on 2016/06/03.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import "Logger.h"
+
+
+@implementation Logger :NSObject
+
+
+-(void)sayOuch:(NSTimer *)t{
+    NSLog(@"Ouch!");
+}
+
+@end
