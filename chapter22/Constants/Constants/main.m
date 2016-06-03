1f5f88cec62bdd0080438b079f42f6cc795235f1
Minami Kyohei <[kyohei.minami@sacn.co.jp]>
2016-06-03 12:07:59 +0900

    chapter22追加

---
+++ chapter22/Constants/Constants/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Constants
+//
+//  Created by Minami Kyohei on 2016/06/03.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        //NSStringのリテラルでは、\uを使って任意のUnicode文字を表す
+        NSLog(@"\u03c0 is %f", M_PI);
+        NSLog(@"%d is larger", MAX(10,12));
+    }
+    return 0;
+}
