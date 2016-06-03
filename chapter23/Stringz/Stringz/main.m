b2f2e1265639d94c3ecf509a8526cb24f1f2463f
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-03 13:12:20 +0900

    コメントを追加

--- chapter23/Stringz/Stringz/main.m
+++ chapter23/Stringz/Stringz/main.m
@@ -10,8 +10,16 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSMutableString *str = [[NSMutableString alloc] init];
+        
+        for(int i = 0; i < 10; i++){
+            [str appendString:@"Aaron is cool!\n"];//文字を追加する
+        }
+        //NSMutableString の writeToFileメソッドでファイルパスを指定
+        [str writeToFile:@"/tmp/cool.txt"
+              atomically:YES encoding:NSUTF8StringEncoding error:NULL];
+        
+        NSLog(@"dane writing /tmp/cool.txt");
     }
     return 0;
 }
