6d57df4da7bc6dc242a064787442fb35ffac69af
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-06 16:38:29 +0900

    chapter32作成 ブロック変数作成

---
+++ chapter32/Block/Block/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Block
+//
+//  Created by Minami Kyohei on 2016/06/06.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
