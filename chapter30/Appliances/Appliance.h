e69141700b19f3a43a85e5fb4b3797d5463f30e6
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-06 15:16:10 +0900

    プロパティ属性やメモリ管理について

--- chapter30/Appliances/Appliance.h
+++ chapter30/Appliances/Appliance.h
@@ -16,8 +16,25 @@
 
 
 @property (copy) NSString *productName;
+
+//プロパティにreadwriteまたはreadonlyのどちらかを指定できる
+//デフォルトはreadwrite
+//setterメソッドを生成したくない場合はreadonlyを指定する
 @property int voltage;
 - (id) initWithProductname:(NSString *)pn;
 
+
+/* week strong test
+__strong NSString *strongString = [[NSString alloc] initWithFormat:@"テスト"];
+__weak   NSString *weakString   = strongString;
+NSLog(@"strong: %@ , weak: %@", strongString, weakString); // strong: テスト , weak: テスト
+// 「テスト」が解放される
+strongString = [[NSString alloc] initWithFormat:@"あいう"];
+NSLog(@"strong: %@ , weak: %@", strongString, weakString); // strong: あいう , weak: nil
+
+
+__weak NSString *weakString2 = [[NSString alloc] initWithFormat:@"あいう"];
+NSLog(@"%@", weakString2); // 代入した瞬間に解放されるので、nilが出力される
+*/
 
 @end
