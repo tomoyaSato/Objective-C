1cc7e364b39715f0bd4e50b304a1fa0b4b32abe1
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-08 17:20:59 +0900

    参照の強さ実験 NSArrayでもうまくいかず

--- review/memory/memory/main.m
+++ review/memory/memory/main.m
@@ -11,7 +11,7 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-    
+    /*
         __strong NSString *strong1 = [[NSString alloc] initWithFormat:@"りんご"];
         __weak      NSString *weak1   = strong1;
         // strong りんご / weak りんご
@@ -22,7 +22,7 @@ int main(int argc, const char * argv[]) {
          // strong みかん / weak nil
         NSLog(@"strong: %@ , weak: %@", strong1, weak1);
         
-        /*------*/
+     
         
         __strong NSString *strong2 = [[NSString alloc] initWithFormat:@"Apple"];
         __weak   NSString *weak2   = strong2;
@@ -33,8 +33,28 @@ int main(int argc, const char * argv[]) {
         strong2 = [[NSString alloc] initWithFormat:@"Orange"];
         // strong Apple / weak Orange
         NSLog(@"strong: %@ , weak: %@", strong2, weak2);
-
-    
+    */
+        
+        
+        __strong NSArray *strongArray = @[@"Monday",@"Tuesday"];
+        __weak NSArray *weakArray = strongArray;
+        // strong : Monday Tuesday / weak Monday Tuesday
+        NSLog(@"%@ : %@", strongArray, weakArray);
+        /*
+        for (int i = 0; i < strongArray.count; i++) {
+            NSLog(@"%@ : %@", strongArray[i], weakArray[i]);
+        }
+        */
+         
+        //解放
+        strongArray = @[@"Wednesday",@"Thursday"];
+        // strong : Wednesday Thursday  / weak Monday Tuesday
+        NSLog(@"%@ : %@", strongArray, weakArray);
+        /*
+        for (int i = 0; i < strongArray.count; i++) {
+            NSLog(@"%@ : %@", strongArray[i], weakArray[i]);
+        }
+        */
     }
     return 0;
 }
